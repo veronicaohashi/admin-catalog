d44b9176644fd8e081bc41e328a82c1ccae23e0f
veronica ohashi <veronica.ohashi@outlook.com>
2023-07-10 16:15:50 -0300

    chore: add spring boot into infra layer

--- infraestructure/src/main/java/com/fullcycle/admin/catalog/infraestructure/Main.java
+++ infraestructure/src/main/java/com/fullcycle/admin/catalog/infraestructure/Main.java
@@ -1,6 +1,13 @@
 package com.fullcycle.admin.catalog.infraestructure;
 
+import com.fullcycle.admin.catalog.infraestructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infraestructure/src/main/java/com/fullcycle/admin/catalog/infraestructure/configuration/WebServerConfig.java
@@ -0,0 +1,10 @@
+package com.fullcycle.admin.catalog.infraestructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("com.fullcycle.admin.catalog")
+public class WebServerConfig {
+}
