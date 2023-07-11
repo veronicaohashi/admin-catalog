ea0e02b9b6b6103b57a17cb295ad37f46c8368cc
veronica ohashi <veronica.ohashi@outlook.com>
2023-07-11 11:21:14 -0300

    chore: configuring spring boot profiles

--- infraestructure/src/main/java/com/fullcycle/admin/catalog/infraestructure/Main.java
+++ infraestructure/src/main/java/com/fullcycle/admin/catalog/infraestructure/Main.java
@@ -3,11 +3,13 @@ package com.fullcycle.admin.catalog.infraestructure;
 import com.fullcycle.admin.catalog.infraestructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
