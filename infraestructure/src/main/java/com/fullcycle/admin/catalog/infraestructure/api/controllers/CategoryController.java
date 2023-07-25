f196543df3593d15b8591ca1f8ca6bb4c1b1d750
veronica ohashi <veronica.ohashi@outlook.com>
2023-07-25 09:41:35 -0300

    chore: initial structure of category controller

---
+++ infraestructure/src/main/java/com/fullcycle/admin/catalog/infraestructure/api/controllers/CategoryController.java
@@ -0,0 +1,17 @@
+package com.fullcycle.admin.catalog.infraestructure.api.controllers;
+
+import com.fullcycle.admin.catalog.domain.pagination.Pagination;
+import com.fullcycle.admin.catalog.infraestructure.api.CategoryAPI;
+import org.springframework.http.ResponseEntity;
+
+public class CategoryController implements CategoryAPI {
+    @Override
+    public ResponseEntity<?> create() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> list(String search, int page, int perPage, String sort, String direction) {
+        return null;
+    }
+}
