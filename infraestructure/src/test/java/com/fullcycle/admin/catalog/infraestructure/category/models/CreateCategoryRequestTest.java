c9b03fe9432dd547aa14988f55d46c8a54743130
veronica ohashi <veronica.ohashi@outlook.com>
2023-08-07 09:03:16 -0300

    chore: add test for category serialization

---
+++ infraestructure/src/test/java/com/fullcycle/admin/catalog/infraestructure/category/models/CreateCategoryRequestTest.java
@@ -0,0 +1,35 @@
+package com.fullcycle.admin.catalog.infraestructure.category.models;
+
+import com.fullcycle.admin.catalog.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+@JacksonTest
+class CreateCategoryRequestTest {
+    @Autowired
+    private JacksonTester<CreateCategoryRequest> json;
+
+    @Test
+    void testUnmarshall() throws Exception {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var json = """
+                {
+                  "name": "%s",
+                  "description": "%s",
+                  "is_active": %s
+                }    
+                """.formatted(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive);
+    }
+}
---
+++ infraestructure/src/test/java/com/fullcycle/admin/catalog/infraestructure/category/models/UpdateCategoryRequestTest.java
@@ -0,0 +1,36 @@
+package com.fullcycle.admin.catalog.infraestructure.category.models;
+
+import com.fullcycle.admin.catalog.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+@JacksonTest
+class UpdateCategoryRequestTest {
+
+    @Autowired
+    private JacksonTester<CategoryResponse> json;
+
+    @Test
+    void testUnmarshall() throws Exception {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var json = """
+        {
+          "name": "%s",
+          "description": "%s",
+          "is_active": %s
+        }    
+        """.formatted(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive);
+    }
+}
