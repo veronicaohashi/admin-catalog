24e50ec53f984474ac51836879cc55cf60031ed7
veronica ohashi <veronica.ohashi@outlook.com>
2023-08-28 19:53:18 -0300

    chore: add validation to cast member

--- domain/src/main/java/com/fullcycle/admin/catalog/domain/castmember/CastMemberValidator.java
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/castmember/CastMemberValidator.java
@@ -1,9 +1,13 @@
 package com.fullcycle.admin.catalog.domain.castmember;
 
+import com.fullcycle.admin.catalog.domain.validation.Error;
 import com.fullcycle.admin.catalog.domain.validation.ValidationHandler;
 import com.fullcycle.admin.catalog.domain.validation.Validator;
 
 public class CastMemberValidator extends Validator {
+
+    public static final int NAME_MAX_LENGTH = 255;
+    public static final int NAME_MIN_LENGTH = 3;
     
     private final CastMember castMember;
 
@@ -14,6 +18,32 @@ public class CastMemberValidator extends Validator {
 
     @Override
     public void validate() {
+        checkNameConstraints();
+        checkTypeConstraints();
+    }
 
+    private void checkNameConstraints() {
+        final var name = castMember.getName();
+        if (name == null) {
+            this.validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+
+        if(name.isBlank()) {
+            this.validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+
+        final int nameLength = name.trim().length();
+        if (nameLength < NAME_MIN_LENGTH || nameLength > NAME_MAX_LENGTH) {
+            this.validationHandler().append(new Error("'name' must be between 3 and 255 character"));
+        }
+    }
+
+    private void checkTypeConstraints() {
+        final var type = castMember.getType();
+        if (type == null) {
+            this.validationHandler().append(new Error("'type' should not be null"));
+        }
     }
 }
--- domain/src/test/java/com/fullcycle/admin/catalog/domain/castmember/CastMemberTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalog/domain/castmember/CastMemberTest.java
@@ -1,14 +1,17 @@
 package com.fullcycle.admin.catalog.domain.castmember;
 
+import com.fullcycle.admin.catalog.domain.exception.NotificationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static com.fullcycle.admin.catalog.domain.castmember.CastMemberType.ACTOR;
 
 class CastMemberTest {
 
     @Test
     void givenAValidParams_whenCallsNewMember_thenInstantiateACastMember() {
         final var expectedName = "Vin Diesel";
-        final var expectedType = CastMemberType.ACTOR;
+        final var expectedType = ACTOR;
 
         final var castmember = CastMember.newMember(expectedName, expectedType);
 
@@ -18,5 +21,60 @@ class CastMemberTest {
         Assertions.assertEquals(expectedType, castmember.getType());
         Assertions.assertNotNull(castmember.getCreatedAt());
         Assertions.assertNotNull(castmember.getUpdatedAt());
+    }
+
+    @Test
+    void givenAInvalidNullName_whenCallsNewMember_thenReceiveANotification() {
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var exception = Assertions.assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(null, ACTOR)
+        );
+
+        Assertions.assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+    }
+
+    @Test
+    void givenAInvalidEmptyName_whenCallsNewMember_thenReceiveANotification() {
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var exception = Assertions.assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember("", ACTOR)
+        );
+
+        Assertions.assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+    }
+
+    @Test
+    void givenAInvalidNameWithLengthMoreThan255_whenCallsNewMember_thenReceiveANotification() {
+        final var invalidName = """
+                A certificação de metodologias que nos auxiliam a lidar com a crescente influência da mídia talvez venha
+                 a ressaltar a relatividade do processo de comunicação como um todo. Todas estas questões, devidamente 
+                 ponderadas, levantam dúvidas sobre se o surgimento do comércio virtual promove a alavancagem do sistema 
+                 de participação geral.                                                                                     
+                """;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 character";
+
+        final var exception = Assertions.assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember(invalidName, ACTOR)
+        );
+
+        Assertions.assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+    }
+
+    @Test
+    void givenAInvalidNullType_whenCallsNewMember_thenReceiveANotification() {
+        final var expectedErrorMessage = "'type' should not be null";
+
+        final var exception = Assertions.assertThrows(
+                NotificationException.class,
+                () -> CastMember.newMember("Vin Diesel", null)
+        );
+
+        Assertions.assertEquals(expectedErrorMessage, exception.getErrors().get(0).message());
+
     }
 }
