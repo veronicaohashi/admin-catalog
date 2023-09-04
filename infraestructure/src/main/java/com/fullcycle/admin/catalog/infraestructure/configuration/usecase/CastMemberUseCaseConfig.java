97b5c14ec10492971ea707abbd51c6c5e402d189
veronica ohashi <veronica.ohashi@outlook.com>
2023-09-04 09:56:13 -0300

    chore: injecting the gateway implementation in the usecase

---
+++ infraestructure/src/main/java/com/fullcycle/admin/catalog/infraestructure/configuration/usecase/CastMemberUseCaseConfig.java
@@ -0,0 +1,53 @@
+package com.fullcycle.admin.catalog.infraestructure.configuration.usecase;
+
+import com.fullcycle.admin.catalog.application.castmember.create.CreateCastMemberUseCase;
+import com.fullcycle.admin.catalog.application.castmember.create.DefaultCreateCastMemberUseCase;
+import com.fullcycle.admin.catalog.application.castmember.delete.DefaultDeleteCastMemberUseCase;
+import com.fullcycle.admin.catalog.application.castmember.delete.DeleteCastMemberUseCase;
+import com.fullcycle.admin.catalog.application.castmember.retrieve.get.DefaultGetCastMemberByIdUseCase;
+import com.fullcycle.admin.catalog.application.castmember.retrieve.get.GetCastMemberByIdUseCase;
+import com.fullcycle.admin.catalog.application.castmember.retrieve.list.DefaultListCastMembersUseCase;
+import com.fullcycle.admin.catalog.application.castmember.retrieve.list.ListCastMembersUseCase;
+import com.fullcycle.admin.catalog.application.castmember.update.DefaultUpdateCastMemberUseCase;
+import com.fullcycle.admin.catalog.application.castmember.update.UpdateCastMemberUseCase;
+import com.fullcycle.admin.catalog.domain.castmember.CastMemberGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class CastMemberUseCaseConfig {
+
+    private final CastMemberGateway castMemberGateway;
+
+    public CastMemberUseCaseConfig(final CastMemberGateway castMemberGateway) {
+        this.castMemberGateway = Objects.requireNonNull(castMemberGateway);
+    }
+
+    @Bean
+    public CreateCastMemberUseCase createCastMemberUseCase() {
+        return new DefaultCreateCastMemberUseCase(castMemberGateway);
+    }
+
+    @Bean
+    public DeleteCastMemberUseCase deleteCastMemberUseCase() {
+        return new DefaultDeleteCastMemberUseCase(castMemberGateway);
+    }
+
+    @Bean
+    public GetCastMemberByIdUseCase getCastMemberByIdUseCase() {
+        return new DefaultGetCastMemberByIdUseCase(castMemberGateway);
+    }
+
+    @Bean
+    public ListCastMembersUseCase listCastMembersUseCase() {
+        return new DefaultListCastMembersUseCase(castMemberGateway);
+    }
+
+    @Bean
+    public UpdateCastMemberUseCase updateCastMemberUseCase() {
+        return new DefaultUpdateCastMemberUseCase(castMemberGateway);
+    }
+}
+
