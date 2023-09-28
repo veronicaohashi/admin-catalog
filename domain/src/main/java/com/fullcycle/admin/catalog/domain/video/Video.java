d403020629f2c599124a26cbc898c0f39323f621
veronica ohashi <veronica.ohashi@outlook.com>
2023-09-28 09:01:47 +0100

    chore: add update video method

--- domain/src/main/java/com/fullcycle/admin/catalog/domain/video/Video.java
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/video/Video.java
@@ -9,7 +9,6 @@ import com.fullcycle.admin.catalog.domain.validation.ValidationHandler;
 
 import java.time.Instant;
 import java.time.Year;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -74,7 +73,6 @@ public class Video extends AggregateRoot<VideoID> {
         this.categories = categories;
         this.genres = genres;
         this.castMembers = members;
-
     }
 
     @Override
@@ -141,139 +139,140 @@ public class Video extends AggregateRoot<VideoID> {
         );
     }
 
-    public String getTitle() {
-        return title;
+    public Video update(
+            final String title,
+            final String description,
+            final Year launchedAt,
+            final double duration,
+            final Rating rating,
+            final boolean opened,
+            final boolean published,
+            final Set<CategoryID> categories,
+            final Set<GenreID> genres,
+            final Set<CastMemberID> members
+    ) {
+        this.title = title;
+        this.description = description;
+        this.launchedAt = launchedAt;
+        this.duration = duration;
+        this.rating = rating;
+        this.opened = opened;
+        this.published = published;
+        this.updatedAt = InstantUtils.now();
+        this.setCategories(categories);
+        this.setGenres(genres);
+        this.setCastMembers(members);
+
+        return this;
     }
 
-    public void setTitle(final String title) {
-        this.title = title;
+    public String getTitle() {
+        return title;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(final String description) {
-        this.description = description;
-    }
-
     public Year getLaunchedAt() {
         return launchedAt;
-    }
-
-    public void setLaunchedAt(final Year launchedAt) {
-        this.launchedAt = launchedAt;
     }
 
     public double getDuration() {
         return duration;
     }
 
-    public void setDuration(final double duration) {
-        this.duration = duration;
-    }
-
     public Rating getRating() {
         return rating;
-    }
-
-    public void setRating(final Rating rating) {
-        this.rating = rating;
     }
 
     public boolean isOpened() {
         return opened;
     }
 
-    public void setOpened(final boolean opened) {
-        this.opened = opened;
-    }
-
     public boolean isPublished() {
         return published;
-    }
-
-    public void setPublished(final boolean published) {
-        this.published = published;
     }
 
     public Instant getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(final Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
     public Instant getUpdatedAt() {
         return updatedAt;
-    }
-
-    public void setUpdatedAt(final Instant updatedAt) {
-        this.updatedAt = updatedAt;
     }
 
     public Optional<ImageMedia> getBanner() {
         return Optional.ofNullable(banner);
     }
 
-    public void setBanner(final ImageMedia banner) {
-        this.banner = banner;
-    }
-
     public Optional<ImageMedia> getThumbnail() {
         return Optional.ofNullable(thumbnail);
-    }
-
-    public void setThumbnail(final ImageMedia thumbnail) {
-        this.thumbnail = thumbnail;
     }
 
     public Optional<ImageMedia> getThumbnailHalf() {
         return Optional.ofNullable(thumbnailHalf);
     }
 
-    public void setThumbnailHalf(final ImageMedia thumbnailHalf) {
-        this.thumbnailHalf = thumbnailHalf;
-    }
-
     public Optional<AudioVideoMedia> getTrailer() {
         return Optional.ofNullable(trailer);
-    }
-
-    public void setTrailer(final AudioVideoMedia trailer) {
-        this.trailer = trailer;
     }
 
     public Optional<AudioVideoMedia> getVideo() {
         return Optional.ofNullable(video);
     }
 
-    public void setVideo(final AudioVideoMedia video) {
-        this.video = video;
-    }
-
     public Set<CategoryID> getCategories() {
         return categories != null ? unmodifiableSet(categories) : emptySet();
-    }
-
-    public void setCategories(final Set<CategoryID> categories) {
-        this.categories = categories != null ? new HashSet<>(categories) : emptySet();
     }
 
     public Set<GenreID> getGenres() {
         return genres != null ? unmodifiableSet(genres) : emptySet();
     }
 
-    public void setGenres(final Set<GenreID> genres) {
-        this.genres = genres != null ? new HashSet<>(genres) : emptySet();
-    }
-
     public Set<CastMemberID> getCastMembers() {
         return castMembers != null ? unmodifiableSet(castMembers) : emptySet();
     }
 
-    public void setCastMembers(final Set<CastMemberID> castMembers) {
+    public Video setBanner(final ImageMedia banner) {
+        this.banner = banner;
+        this.updatedAt = InstantUtils.now();
+        return this;
+    }
+
+    public Video setThumbnail(final ImageMedia thumbnail) {
+        this.thumbnail = thumbnail;
+        this.updatedAt = InstantUtils.now();
+        return this;
+    }
+
+    public Video setThumbnailHalf(final ImageMedia thumbnailHalf) {
+        this.thumbnailHalf = thumbnailHalf;
+        this.updatedAt = InstantUtils.now();
+        return this;
+    }
+
+    public Video setTrailer(final AudioVideoMedia trailer) {
+        this.trailer = trailer;
+        this.updatedAt = InstantUtils.now();
+        return this;
+    }
+
+    public Video setVideo(final AudioVideoMedia video) {
+        this.video = video;
+        this.updatedAt = InstantUtils.now();
+        return this;
+    }
+
+    private void setCategories(final Set<CategoryID> categories) {
+        this.categories = categories != null ? new HashSet<>(categories) : emptySet();
+    }
+
+    private void setGenres(final Set<GenreID> genres) {
+        this.genres = genres != null ? new HashSet<>(genres) : emptySet();
+    }
+
+    private void setCastMembers(final Set<CastMemberID> castMembers) {
         this.castMembers = castMembers != null ? new HashSet<>(castMembers) : emptySet();
     }
 }
--- domain/src/test/java/com/fullcycle/admin/catalog/domain/video/VideoTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalog/domain/video/VideoTest.java
@@ -8,12 +8,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.Year;
+import java.util.Collections;
 import java.util.Set;
 
 class VideoTest {
 
     @Test
-    void givenValidParams_whenCallNewVideo_thenInstantiateAVideo(){
+    void givenValidParams_whenCallNewVideo_thenInstantiateAVideo() {
         final var expectedTitle = "System Design Interviews";
         final var expectedDescription = """
                 Disclaimer: o estudo de caso apresentado tem fins educacionais e representa nossas opiniões pessoais.
@@ -22,7 +23,7 @@ class VideoTest {
                 https://imersao.fullcycle.com.br/
                 """;
         final var expectedLaunchedAt = Year.of(2023);
-        final var expectedDuration= 120.10;
+        final var expectedDuration = 120.10;
         final var expectedOpened = false;
         final var expectedPublished = false;
         final var expectedRating = Rating.L;
@@ -60,5 +61,210 @@ class VideoTest {
         Assertions.assertTrue(video.getThumbnail().isEmpty());
         Assertions.assertTrue(video.getThumbnailHalf().isEmpty());
         Assertions.assertDoesNotThrow(() -> video.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    void givenValidVideo_whenCallsUpdate_thenReturnUpdated() {
+        final var expectedTitle = "System Design Interviews";
+        final var expectedDescription = """
+                Disclaimer: o estudo de caso apresentado tem fins educacionais e representa nossas opiniões pessoais.
+                Esse vídeo faz parte da Imersão Full Stack && Full Cycle.
+                Para acessar todas as aulas, lives e desafios, acesse:
+                https://imersao.fullcycle.com.br/
+                """;
+        final var expectedLaunchedAt = Year.of(2023);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var video = Video.newVideo(
+                "Test Title",
+                "Test Description",
+                Year.of(1999),
+                10,
+                Rating.AGE_10,
+                true,
+                true,
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptySet()
+        );
+
+        final var updatedVideo = Video.with(video).update(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        Assertions.assertNotNull(video.getId());
+        Assertions.assertEquals(expectedTitle, updatedVideo.getTitle());
+        Assertions.assertEquals(expectedDescription, updatedVideo.getDescription());
+        Assertions.assertEquals(expectedLaunchedAt, updatedVideo.getLaunchedAt());
+        Assertions.assertEquals(expectedDuration, updatedVideo.getDuration());
+        Assertions.assertEquals(expectedOpened, updatedVideo.isOpened());
+        Assertions.assertEquals(expectedPublished, updatedVideo.isPublished());
+        Assertions.assertEquals(expectedRating, updatedVideo.getRating());
+        Assertions.assertEquals(expectedCategories, updatedVideo.getCategories());
+        Assertions.assertEquals(expectedGenres, updatedVideo.getGenres());
+        Assertions.assertEquals(expectedMembers, updatedVideo.getCastMembers());
+        Assertions.assertEquals(video.getCreatedAt(), updatedVideo.getCreatedAt());
+        Assertions.assertTrue(updatedVideo.getUpdatedAt().isAfter(video.getUpdatedAt()));
+        Assertions.assertTrue(updatedVideo.getVideo().isEmpty());
+        Assertions.assertTrue(updatedVideo.getTrailer().isEmpty());
+        Assertions.assertTrue(updatedVideo.getBanner().isEmpty());
+        Assertions.assertTrue(updatedVideo.getThumbnail().isEmpty());
+        Assertions.assertTrue(updatedVideo.getThumbnailHalf().isEmpty());
+
+        Assertions.assertDoesNotThrow(() -> updatedVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    void givenValidVideo_whenCallsSetVideo_thenReturnUpdated() {
+        final var expectedVideo = AudioVideoMedia.with(
+                "abc",
+                "Video.mp4",
+                "/videos",
+                "",
+                MediaStatus.PENDING
+        );
+        final var video = Video.newVideo(
+                "Test Title",
+                "Test Description",
+                Year.of(1999),
+                10,
+                Rating.AGE_10,
+                true,
+                true,
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptySet()
+        );
+
+        final var updatedVideo = Video.with(video).setVideo(expectedVideo);
+
+        Assertions.assertEquals(expectedVideo, updatedVideo.getVideo().get());
+        Assertions.assertTrue(updatedVideo.getUpdatedAt().isAfter(video.getUpdatedAt()));
+        Assertions.assertDoesNotThrow(() -> updatedVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    void givenValidVideo_whenCallsSetTrailer_thenReturnUpdated() {
+        final var expectedTrailer = AudioVideoMedia.with(
+                "abc",
+                "Trailer.mp4",
+                "/trailers",
+                "",
+                MediaStatus.PENDING
+        );
+        final var video = Video.newVideo(
+                "Test Title",
+                "Test Description",
+                Year.of(1999),
+                10,
+                Rating.AGE_10,
+                true,
+                true,
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptySet()
+        );
+
+        final var updatedVideo = Video.with(video).setTrailer(expectedTrailer);
+
+        Assertions.assertEquals(expectedTrailer, updatedVideo.getTrailer().get());
+        Assertions.assertTrue(updatedVideo.getUpdatedAt().isAfter(video.getUpdatedAt()));
+        Assertions.assertDoesNotThrow(() -> updatedVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    void givenValidVideo_whenCallsSetBanner_thenReturnUpdated() {
+        final var expectedBanner = ImageMedia.with(
+                "abc",
+                "Banner.jpg",
+                "/banners"
+        );
+        final var video = Video.newVideo(
+                "Test Title",
+                "Test Description",
+                Year.of(1999),
+                10,
+                Rating.AGE_10,
+                true,
+                true,
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptySet()
+        );
+
+        final var updatedVideo = Video.with(video).setBanner(expectedBanner);
+
+        Assertions.assertEquals(expectedBanner, updatedVideo.getBanner().get());
+        Assertions.assertTrue(updatedVideo.getUpdatedAt().isAfter(video.getUpdatedAt()));
+        Assertions.assertDoesNotThrow(() -> updatedVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    void givenValidVideo_whenCallsSetThumbnail_thenReturnUpdated() {
+        final var expectedThumbnail = ImageMedia.with(
+                "abc",
+                "Thumbnail.jpg",
+                "/thumbnails"
+        );
+        final var video = Video.newVideo(
+                "Test Title",
+                "Test Description",
+                Year.of(1999),
+                10,
+                Rating.AGE_10,
+                true,
+                true,
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptySet()
+        );
+
+        final var updatedVideo = Video.with(video).setThumbnail(expectedThumbnail);
+
+        Assertions.assertEquals(expectedThumbnail, updatedVideo.getThumbnail().get());
+        Assertions.assertTrue(updatedVideo.getUpdatedAt().isAfter(video.getUpdatedAt()));
+        Assertions.assertDoesNotThrow(() -> updatedVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    void givenValidVideo_whenCallsSetThumbnailHalf_tehnReturnUpdated() {
+        final var expectedThumbnailHalf = ImageMedia.with(
+                "abc",
+                "ThumbnailHalf.jpg",
+                "/thumbnailHalf"
+        );
+        final var video = Video.newVideo(
+                "Test Title",
+                "Test Description",
+                Year.of(1999),
+                10,
+                Rating.AGE_10,
+                true,
+                true,
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptySet()
+        );
+
+        final var updatedVideo = Video.with(video).setThumbnailHalf(expectedThumbnailHalf);
+
+        Assertions.assertEquals(expectedThumbnailHalf, updatedVideo.getThumbnailHalf().get());
+        Assertions.assertTrue(updatedVideo.getUpdatedAt().isAfter(video.getUpdatedAt()));
+        Assertions.assertDoesNotThrow(() -> updatedVideo.validate(new ThrowsValidationHandler()));
     }
 }
