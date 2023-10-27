87915d1116701c0343a3e263022575dcf1eebc11
veronica ohashi <veronica.ohashi@outlook.com>
2023-10-27 09:22:15 +0100

    chore: create qualifier for beans

--- infraestructure/src/main/java/com/fullcycle/admin/catalog/infraestructure/configuration/AmqpConfig.java
+++ infraestructure/src/main/java/com/fullcycle/admin/catalog/infraestructure/configuration/AmqpConfig.java
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalog.infraestructure.configuration;
 
+import com.fullcycle.admin.catalog.infraestructure.configuration.annotations.VideoCreatedQueue;
+import com.fullcycle.admin.catalog.infraestructure.configuration.annotations.VideoEncodedQueue;
 import com.fullcycle.admin.catalog.infraestructure.configuration.properties.QueueProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,12 +12,14 @@ public class AmqpConfig {
 
     @Bean
     @ConfigurationProperties("amqp.queues.video-created")
+    @VideoCreatedQueue
     QueueProperties videoCreatedProperties() {
         return new QueueProperties();
     }
 
     @Bean
     @ConfigurationProperties("amqp.queues.video-encoded")
+    @VideoEncodedQueue
     QueueProperties videoEncodedProperties() {
         return new QueueProperties();
     }
